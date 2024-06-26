2742026b09c03b06c3bebfa1e5cfd1d7d290c305
Triveni Nath Upadhyay <88548049+TriveniG5@users.noreply.github.com>
2024-06-26 02:20:51 +0000

    running project

---
+++ Projectset4/guess_number.js
@@ -0,0 +1,28 @@
+const randomNumber=parseInt(Math.random()*100 +1);
+
+const submi=document.querySelector(`subt`);
+const userInput=document.querySelector(`guessField`);
+const guessSlot=document.querySelector(`.guesses`);
+const remaining=document.querySelector(`lastresult`);
+const lowOrHi=document.querySelector(`lowOrHi`);
+const startOver=document.createElement(`.resultParas`);
+
+const p= document.createElement(`p`);
+
+let prevGuess=[];
+
+let numGuess=1;
+
+let playGame=true;
+
+function validateGuess(guess){
+
+}
+
+ function checkGuess(guess){
+
+ }
+
+ function displayMessage(guess){
+    
+ }
